b2770e50cc6c389cdccc06e9c4a030d82bcfdf2e
Nicolas Villamarin <villamarin.nico@gmail>
2023-03-03 17:58:20 -0300

    Agregando base de dato FireStore

--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import ItemList from "../components/ItemList";
 import "../App.css"
 import { useParams } from "react-router-dom";
-import {getProducts}  from "../../data.js"
+import { getProducts } from "../../data.js"
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 import Loading from "../components/Loading";
 
 const ItemListContainer = ({ greeting }) => {
@@ -12,18 +13,22 @@ const ItemListContainer = ({ greeting }) => {
     const [listaProductos, setListaProductos] = useState([]);
 
     useEffect(() => {
-        getProducts()
-            .then((res) => {
+        setLoading(true);
+        const db = getFirestore();
+
+        const itemsCollection = collection(db, "Productos");
+    
+        getDocs(itemsCollection)
+            .then((snapshot) => {
                 if (categoryid) {
-                    setListaProductos(res.filter((item) => item.category === categoryid));
+                    setListaProductos(snapshot.docs.filter((doc) => ({ id: doc.category === categoryid, ...doc.data() }) ));
                 } else {
-                    setListaProductos(res);
+                    setListaProductos(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
                 }
             })
             .catch((error) => console.log(error))
             .finally(() => setLoading(false));
     }, [categoryid]);
-
     return (
         <>
             <h1 className="TituloPrincipal">{greeting}</h1>
